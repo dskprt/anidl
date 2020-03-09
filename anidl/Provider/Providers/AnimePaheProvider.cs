ff00689846e2150e4bfa9d8ba33b51637bdba540
dskprt <59660013+dskprt@users.noreply.github.com>
2020-03-09 19:09:07 +0100

    kwik obfuscation reverse-engineered + url regex

--- anidl/Provider/Providers/AnimePaheProvider.cs
+++ anidl/Provider/Providers/AnimePaheProvider.cs
@@ -4,7 +4,9 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace anidl.Provider.Providers {
@@ -71,24 +73,64 @@ namespace anidl.Provider.Providers {
                         foreach(dynamic o in ob) {
                             foreach(dynamic res in o) {
                                 try {
-                                    client.Headers.Add("Referrer", string.Format("{0}?id={1}&m=embed&p={2}", BASE_API_URL, id, host));
+                                    client.Headers.Add("Referer", "https://animepahe.com/");
+                                    client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:64.0) Gecko/20100101 Firefox/64.0");
+
                                     str = client.DownloadString((string)res.url);
                                     doc.LoadHtml(str);
 
                                     foreach (var node in doc.DocumentNode.SelectNodes("//script")) {
                                         if (node.InnerText.Contains("eval(")) {
                                             string[] arr = node.InnerText.Split('\'');
-                                            MessageBox.Show(arr[arr.Length - 4]);
+                                            string src = GetKwikSource(arr[arr.Length - 4]);
                                         }
                                     }
+
+                                    break;
                                 } catch(WebException e) {
-                                    continue;
+                                    if(((HttpWebResponse)e.Response).StatusCode == HttpStatusCode.NotFound) {
+                                        continue;
+                                    } else {
+                                        MessageBox.Show(e.Message);
+                                    }
                                 }
                             }
+
+                            break;
                         }
+
+                        break;
                     }
+
+                    break;
                 }
             }
+        }
+
+        private string GetKwikSource(string data) {
+            string p = "g m='1r://11-10.Z.Y/X/W/V/U.T?S=R&Q=N';g b=O.C('b');g 0=r M(b,{'L':{'K':s},'J':'16:9','I':5,'H':{'G':'F'},E:['8-D','8','12','P-d','14','13','1m','1n','1o','1p','t'],'t':{'1q':s}});4(!w.1t()){b.1u=m}p{f q={1v:1s};g c=r w(q);c.1i(m);c.1g(b);i.c=c}0.2('1d',7=>{i.1c.1b.1a('19')});0.18=1;j x(a,l,n){4(a.o){a.o(l,n,17)}p 4(a.u){a.u('2'+l,n)}}f 6=j(k){i.B.15(k,'*')};x(i,'k',j(e){f 3=e.3;4(3==='8')0.8();4(3==='h')0.h();4(3==='y')0.y();4(3.1e('d')){f d=1f(3.1h('-')[1]);1j.1k(d);0.z=d}});0.2('A',7=>{6('A')});0.2('8',7=>{6('8')});0.2('h',7=>{6('h')});0.2('1l',7=>{6(0.z)});0.2('v',7=>{6('v')});";
+            int a = 62;
+            int c = 94;
+            string[] k = data.Split('|');
+            int e = 0;
+            Dictionary<string, string> d = new Dictionary<string, string>();
+
+            string One(int _c) {
+                return (_c < a ? "" : One(_c / a)) + ((_c = _c % a) > 35 ?
+                    Convert.ToString(Convert.ToChar(c + 29)) : Base36Converter.ConvertTo(_c));
+            }
+
+            for(c = c - 1; c > 0; c--) {
+                d[One(c)] = (string.IsNullOrEmpty(k[c])) ? One(c) : k[c];
+            }
+
+            for(c = 93; c > 0; c--) {
+                p = Regex.Replace(p, "\\b" + One(c) + "\\b", d[One(c)]);
+            }
+
+            string match = Regex.Match(p, "'[^']+'").Value;
+
+            return match.Substring(1, match.Length - 2);
         }
     }
 }
---
+++ anidl/Utils/Base36Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anidl.Utils {
+
+    public class Base36Converter {
+
+        private const int Base = 36;
+        private const string Chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+
+        public static string ConvertTo(int value) {
+            string result = "";
+
+            while (value > 0) {
+                result = Chars[value % Base] + result; // use StringBuilder for better performance
+                value /= Base;
+            }
+
+            return result;
+        }
+    }
+}
